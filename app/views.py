1ffd57deb8a5a03c8e80f67f3c71e8e905052bbe
mts-mlna <matiasezequielmolina2006@gmail.com>
2025-10-21 22:26:12 -0300

    Creacion de las vistas de inicio, login y subida de sonidos

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.db.models import Q
+from .models import *
 # Create your views here.
+
+def home(request):
+    return render (request, "index.html")
+
+def login(request):
+    return render (request, "registration/login.html")
+
+def nuevosonido(request):
+    return render (request, "pages/nuevo-sonido.html")
